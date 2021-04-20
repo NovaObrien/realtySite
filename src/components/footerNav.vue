db7f6a2cd8aef8ef1a361271a6585c39e6edd09f
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-04-20 12:03:45 -0600

    created a footerNav

---
+++ src/components/footerNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer-nav">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterNav',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
