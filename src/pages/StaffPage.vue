fdeb4da7c162df30a5ccc9a214fdefdfb4fd639c
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-04-29 02:23:28 -0600

    Updated staff img names and staffPage staff

--- src/pages/StaffPage.vue
+++ src/pages/StaffPage.vue
@@ -14,7 +14,7 @@
         </p>
       </div>
     </div>
-    <div class="row">
+    <div class="row mb-4">
       <div class="col-6 mt-3">
         <h6>
           KAROLYN PLEHAL, GRI, ASSOCIATE BROKER
@@ -28,6 +28,114 @@
       </div>
       <div class="col-6">
         <img class="staff-image rounded" src="../assets/img/staff/Karolyn.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col-6">
+        <img class="staff-image rounded shadow image-" src="../assets/img/staff/Dwight.jpg" alt="">
+      </div>
+      <div class="col-6">
+        <h6>Dwight Jivden, GRI, Sales Associate</h6>
+        <p class="small-font mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus cum amet laudantium provident aut temporibus repellendus mollitia optio? Est alias deleniti rerum officia! Ut illum adipisci dolorem ducimus officiis aperiam?
+        </p>
+        <p class="medium-font">
+          Contact: (208) 315-3034, Email: dwight@cascadelakerealty.com
+        </p>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-6 mt-3">
+        <h6>
+          Randy Mead, Realtor Sales Associate
+        </h6>
+        <p class="small-font">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione tempore nobis tempora odio ut, aspernatur praesentium magni corporis nisi voluptatibus.
+        </p>
+        <p class="medium-font">
+          (208) 810-1974, randy@CascadeLakeRealty.com
+        </p>
+      </div>
+      <div class="col-6">
+        <img class="staff-image rounded" src="../assets/img/staff/Randy.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col-6">
+        <img class="staff-image rounded shadow image-" src="../assets/img/staff/Melanie.jpg" alt="">
+      </div>
+      <div class="col-6">
+        <h6>Melanie Hamilton Munson, Realtor®</h6>
+        <p class="small-font mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus cum amet laudantium provident aut temporibus repellendus mollitia optio? Est alias deleniti rerum officia! Ut illum adipisci dolorem ducimus officiis aperiam?
+        </p>
+        <p class="medium-font">
+          (208) 631-0439, melanie@cascadelakerealty.com
+        </p>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-6 mt-3">
+        <h6>
+          Kristen Miller, Realtor®
+        </h6>
+        <p class="small-font">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione tempore nobis tempora odio ut, aspernatur praesentium magni corporis nisi voluptatibus.
+        </p>
+        <p class="medium-font">
+          (208) 315-2168, kristen@CascadeLakeRealty.com
+        </p>
+      </div>
+      <div class="col-6">
+        <img class="staff-image rounded" src="../assets/img/staff/Kristen.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col-6">
+        <img class="staff-image rounded shadow image-" src="../assets/img/staff/Naomi.jpg" alt="">
+      </div>
+      <div class="col-6">
+        <h6>Naomi Rogers, Realtor®</h6>
+        <p class="small-font mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus cum amet laudantium provident aut temporibus repellendus mollitia optio? Est alias deleniti rerum officia! Ut illum adipisci dolorem ducimus officiis aperiam?
+        </p>
+        <p class="medium-font">
+          (208) 469-0640, naomi@cascadelakerealty.com
+        </p>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-6 mt-3">
+        <h6>
+          Diane Linthicum, Realtor®
+        </h6>
+        <p class="small-font">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione tempore nobis tempora odio ut, aspernatur praesentium magni corporis nisi voluptatibus.
+        </p>
+        <p class="medium-font">
+          (208) 217-1671, diane@CascadeLakeRealty.com
+        </p>
+      </div>
+      <div class="col-6">
+        <img class="staff-image rounded" src="../assets/img/staff/Diane.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col-6">
+        <img class="staff-image rounded shadow image-" src="../assets/img/staff/Sue.jpg" alt="">
+      </div>
+      <div class="col-6">
+        <h6>Sue Laughlin, Office Manager®</h6>
+        <p class="small-font mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus cum amet laudantium provident aut temporibus repellendus mollitia optio? Est alias deleniti rerum officia! Ut illum adipisci dolorem ducimus officiis aperiam?
+        </p>
+        <p class="medium-font">
+          (208) 382-4921, sue@cascadelakerealty.com
+        </p>
       </div>
     </div>
   </div>
@@ -50,8 +158,8 @@ export default {
 
 .staff-image{
   object-fit: cover;
-  height: 164px;
-  width: 147px;
+  height: 35vh;
+  width: 45vw;
 }
   .small-font{
     font-size: 10px;
